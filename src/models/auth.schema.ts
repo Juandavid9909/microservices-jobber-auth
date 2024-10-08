3a64879ec75a0dea442afd5b0891b31566809e43
Juan David <juandavid918@gmail.com>
2024-10-08 08:17:45 -0500

    Solución problema de TypeScript con prototypes

--- src/models/auth.schema.ts
+++ src/models/auth.schema.ts
@@ -5,67 +5,78 @@ import { sequelize } from "@auth/database";
 
 const SALT_ROUND = 10;
 
+interface AuthModelInstanceMethods extends Model {
+  prototype: {
+    comparePassword: (password: string, hashedPassword: string) => Promise<boolean>;
+    hashPassword: (password: string) => Promise<string>;
+  };
+}
+
 type AuthUserCreationAttributes = Optional<IAuthDocument, "id" | "createdAt" | "passwordResetToken" | "passwordResetExpires">;
 
-const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> = sequelize.define("auths", {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  profilePublicId: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  country: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  profilePicture: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  emailVerificationToken: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  emailVerified: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: 0
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: Date.now
-  },
-  passwordResetToken: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  passwordResetExpires: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: new Date()
-  }
-}, {
-  indexes: [
-    {
-      unique: true,
-      fields: ["email"]
+const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods = sequelize.define(
+  "auths",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    {
-      unique: true,
-      fields: ["username"]
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profilePublicId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    emailVerificationToken: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    emailVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Date.now
+    },
+    passwordResetToken: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    passwordResetExpires: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
     }
-  ]
-});
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["email"]
+      },
+      {
+        unique: true,
+        fields: ["username"]
+      }
+    ]
+  }
+) as ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods;
 
 AuthModel.addHook("beforeCreate", async (auth: Model) => {
   const hashedPassword: string = await hash(auth.dataValues.password as string, SALT_ROUND);
@@ -73,7 +84,7 @@ AuthModel.addHook("beforeCreate", async (auth: Model) => {
   auth.dataValues.password = hashedPassword;
 });
 
-AuthModel.prototype.comparePassword = async function(password: string, hashedPassword: string): Promise<boolean> {
+AuthModel.prototype.comparePassword = async function (password: string, hashedPassword: string): Promise<boolean> {
   return compare(password, hashedPassword);
 };
 
