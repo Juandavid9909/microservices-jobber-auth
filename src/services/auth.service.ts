e55317e37c5599c66c7dc34da6ea4d5e4eaa818d
Juan David <juandavid918@gmail.com>
2024-10-09 15:34:08 -0500

    Métodos de servicio de autenticación

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { authChannel } from "@auth/server";
 import { AuthModel } from "@auth/models/auth.schema";
-import { IAuthBuyerMessageDetails, IAuthDocument } from "@juandavid9909/jobber-shared";
-import { Model } from "sequelize";
-import { publishDirectMessage } from "@auth/queues/auth.producer";
+import { config } from "@auth/config";
+import { firstLetterUppercase, IAuthBuyerMessageDetails, IAuthDocument, lowerCase } from "@juandavid9909/jobber-shared";
+import { Model, Op } from "sequelize";
 import { omit } from "lodash";
+import { publishDirectMessage } from "@auth/queues/auth.producer";
+import { sign } from "jsonwebtoken";
 
 export const createAuthUser = async (data: IAuthDocument): Promise<IAuthDocument> => {
   const result: Model = await AuthModel.create(data);
@@ -27,4 +29,140 @@ export const createAuthUser = async (data: IAuthDocument): Promise<IAuthDocument
   const userData: IAuthDocument = omit(result.dataValues, ["password"]) as IAuthDocument;
 
   return userData;
+};
+
+export const getAuthUserById = async (authId: number): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      id: authId
+    },
+    attributes: {
+      exclude: ["password"]
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const getUserByUsernameOrEmail = async (username: string, email: string): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      [Op.or]: [
+        {
+          username: firstLetterUppercase(username)
+        },
+        {
+          email: lowerCase(email)
+        }
+      ]
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const getUserByUsername = async (username: string): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      username: firstLetterUppercase(username)
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const getUserByEmail = async (email: string): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      email: lowerCase(email)
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const getAuthUserByVerificationToken = async (token: string): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      emailVerificationToken: token
+    },
+    attributes: {
+      exclude: ["password"]
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const getAuthUserByPasswordToken = async (token: string): Promise<IAuthDocument> => {
+  const user: Model = await AuthModel.findOne({
+    where: {
+      [Op.and]: [
+        {
+          passwordResetToken: token
+        },
+        {
+          passwordResetExpires: {
+            [Op.gt]: new Date()
+          }
+        }
+      ]
+    }
+  }) as Model;
+
+  return user.dataValues;
+};
+
+export const updateVerifyEmailField = async (authId: number, emailVerified: number, emailVerificationToken: string): Promise<void> => {
+  await AuthModel.update(
+    {
+      emailVerified,
+      emailVerificationToken
+    },
+    {
+      where: {
+        id: authId
+      }
+    }
+  );
+};
+
+export const updatePasswordToken = async (authId: number, token: string, tokenExpiration: Date): Promise<void> => {
+  await AuthModel.update(
+    {
+      passwordResetToken: token,
+      passwordResetExxpires: tokenExpiration
+    },
+    {
+      where: {
+        id: authId
+      }
+    }
+  );
+};
+
+export const updatePassword = async (authId: number, password: string): Promise<void> => {
+  await AuthModel.update(
+    {
+      password,
+      passwordResetToken: "",
+      passwordResetExxpires: new Date()
+    },
+    {
+      where: {
+        id: authId
+      }
+    }
+  );
+};
+
+export const signToken = (id: number, email: string, username: string): string => {
+  return sign(
+    {
+      id,
+      email,
+      username
+    },
+    config.JWT_TOKEN!
+  );
 };
