6c3c5fcb37035abcabed01272287ec7dca1552d2
Juan David <juandavid918@gmail.com>
2024-11-06 10:38:28 -0500

    Método SignIn y validación de token

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { create } from "@auth/controllers/signup";
+import { read } from "@auth/controllers/signin";
 import express, { Router } from "express";
 
 const router: Router = express.Router();
 
 export const authRoutes = (): Router => {
   router.post("/signup", create);
+  router.post("/signin", read);
 
   return router;
 };
